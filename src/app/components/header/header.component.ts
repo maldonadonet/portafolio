4da84ba1a8f7cc2e2a52aadf1e1c56845c2e3fc3
maldonado.net <maldonado.net@hotmail.com>
2018-05-24 15:09:45 -0500

    Primera version

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { InformacionService } from "../../services/informacion.service";
-
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -8,8 +8,13 @@ import { InformacionService } from "../../services/informacion.service";
 })
 export class HeaderComponent {
 
-    constructor( public _is : InformacionService){
-        
+    constructor( public _is : InformacionService,
+                private router:Router){
+
     }
 
+    public buscar_producto( termino:string ){
+        
+        this.router.navigate( ['buscar',termino ]);
+    }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,8 +4,10 @@ import { Http } from "@angular/http";
 
 @Injectable()
 export class ProductosService {
-
+    // Variable para los productos de firebase
     productos : any[] = [];
+    // Variable para los productos filtrados
+    productos_filtrado : any[] = [];
     // Variable para el loading
     cargando : boolean = true;
 
@@ -16,15 +18,61 @@ export class ProductosService {
     public cargar_productos(){
 
         this.cargando = true;
-        this.http.get("https://portafolio-6c3f1.firebaseio.com/productos_idx.json")
-                .subscribe( res =>{
-                    //console.log(res.json());
-                    setTimeout( ()=>{
-                        this.productos = res.json();
-                        this.cargando = false;
-                    },1500)
+
+        let promesa = new Promise( (resolve, reject)=>{
+
+            this.http.get("https://portafolio-6c3f1.firebaseio.com/productos_idx.json")
+                    .subscribe( res =>{
+                        //console.log(res.json());
+                        //setTimeout( ()=>{
+                            this.cargando = false;
+                            this.productos = res.json();
+                                resolve();
+                        //},1500)
+                    });
+
                 })
 
+            return promesa;
+        }
+
+    public cargar_producto( cod:string){
+        return this.http.get(`https://portafolio-6c3f1.firebaseio.com/productos/${cod}.json`);
     }
+
+    public buscar_producto( termino : string ){
+
+        // console.log("Buscando producto");
+        // console.log(this.productos.length );
+
+        if( this.productos.length === 0){
+            this.cargar_productos().then( ()=>{
+                // Termino la carga
+                this.filtrar_productos(termino);
+            });
+        }else{
+            this.filtrar_productos(termino);
+        }
+
+
+    }
+
+    private filtrar_productos( termino:string ){
+
+        this.productos_filtrado = [];
+
+        termino = termino.toLowerCase();
+
+        this.productos.forEach( prod =>{
+
+            if( prod.categoria.indexOf( termino ) >= 0 || prod.titulo.toLowerCase().indexOf( termino ) >= 0 ){
+                this.productos_filtrado.push(prod);
+            }
+
+
+        })
+    }
+
+
 
 }
