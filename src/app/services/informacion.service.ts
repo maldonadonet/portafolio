5c7602b3382bbd29c54ddda1f712f1bfc071db16
maldonado.net <maldonado.net@hotmail.com>
2018-05-24 00:22:18 -0500

    metodo en servicio implementado de firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,12 +5,28 @@ import { Http } from "@angular/http";
 export class InformacionService {
     info : any = {};
     cargada : boolean = false;
+    cargada_sobre_nosotros : boolean = false;
+    equipo : any = [];
 
   constructor( public http : Http) {
-      this.http.get("assets/data/info.pagina.json")
+      this.carga_info();
+      this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
+      this.http.get("https://portafolio-6c3f1.firebaseio.com/equipo.json")
                 .subscribe( data=>{
                     console.log(data.json());
                     this.cargada = true;
+                    this.equipo = data.json();
+                })
+  }
+
+  public carga_sobre_nosotros(){
+      this.http.get("assets/data/info.pagina.json")
+                .subscribe( data=>{
+                    //console.log(data.json());
+                    this.cargada_sobre_nosotros = true;
                     this.info = data.json();
                 })
   }
