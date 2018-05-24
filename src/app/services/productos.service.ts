2c0be32ae70f11493b37cc6a21357c4ae35f9c18
maldonado.net <maldonado.net@hotmail.com>
2018-05-24 12:02:32 -0500

    Carga de imagenes desde firebase

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,6 +19,7 @@ export class ProductosService {
         this.http.get("https://portafolio-6c3f1.firebaseio.com/productos_idx.json")
                 .subscribe( res =>{
                     console.log(res.json());
+                    this.productos = res.json();
                     this.cargando = false;
                 })
 
